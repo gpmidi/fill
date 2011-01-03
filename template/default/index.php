f9de6d859c0f98dd6933736fa1d06b21f9398eec
Ryan9104 <Ryan9104@gmail.com>
2011-01-03 15:03:31 -0600

    tits

--- template/default/index.php
+++ template/default/index.php
@@ -43,14 +43,20 @@ $gitCommit = unserialize(file_get_contents(HR_ROOT . '/gitcommit.txt'));
 		</div>
 	        <div class="featured-rotator">
 	            <div class="featured-rotator-wrap">
-	                <a href="#prev" class="rprev"></a>
-	                <ul>
-	                    <li>featured item 1</li>
-	                    <li>featured item 2</li>
-	                    <li>featured item 3</li>
-	                    <li>featured item 4</li>
-	                </ul>
-	                <a href="#next" class="rnext"></a>
+					<div class="featured">
+						<a href="#prev" class="rprev"></a>
+					</div>
+					<div class="featured">
+						<ul>
+							<li>featured item 1</li>
+							<li>featured item 2</li>
+							<li>featured item 3</li>
+							<li>featured item 4</li>
+						</ul>
+					</div>
+					<div class="featured">
+						<a href="#next" class="rnext"></a>
+					</div>
 	            </div>
 	        </div>
 		<div class="content-wrap">
