63bb2731a72ef7bc1cc0ca9930fe26afbfdbfd56
Luke Granger-Brown <github@lukegb.com>
2011-01-03 20:02:35 +0000

    LOL

--- pub/hudsonBuildTrigger.php
+++ pub/hudsonBuildTrigger.php
@@ -4,4 +4,6 @@ if (!isset($_GET['buildToTrigger'])) {
   exit('No build specified');
 }
 
-exit(file_get_contents('http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build'));
+echo 'Fetching: '.'http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build - ...<br /><br />';
+
+echo file_get_contents('http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build');
