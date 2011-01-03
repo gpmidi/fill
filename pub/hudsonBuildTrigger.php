00e222386f4517cd96b930fd3fc10def703cdd4d
Luke Granger-Brown <github@lukegb.com>
2011-01-03 20:07:04 +0000

    Hudson Trigger

--- pub/hudsonBuildTrigger.php
+++ pub/hudsonBuildTrigger.php
@@ -3,7 +3,13 @@
 if (!isset($_GET['buildToTrigger'])) {
   exit('No build specified');
 }
+// Get reason from $_POST['payload']
+
+$payload = json_decode($_POST['payload']);
+$reason = 'Build started due to source change: ' . $payload['before'] . ' -> ' . $payload['after'];
+
+$reason = urlencode($reason);
 
 echo 'Fetching: '.'http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build - ...<br /><br />';
 
-echo file_get_contents('http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build?token=BUILDME123');
+file_get_contents('http://hudson.lukegb.com/job/'.urlencode($_GET['buildToTrigger']).'/build?token=BUILDME123&reason='.$reason);
