68f9d130fd71afe9cc3d9c84d35ba60e2afcff37
Luke GB <github@lukegb.com>
2011-01-04 17:36:32 +0000

    This is now the stable branch.

--- pages/faq.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$template_settings=array();
-	$template_settings['HR_TEMPLATE_TITLE'] = "Frequently Asked Questions";
-	$template_settings['HR_TEMPLATE_VARS'] = array('url' => '/faq', 'uri' => 'faq');
